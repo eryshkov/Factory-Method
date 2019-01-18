5c635bea6f0407cbcee82ce4811a54f65cac069b
Evgeniy Ryshkov <eryshkov@gmail.com>
2019-01-18 13:14:30 +0300

    Exercise & Jumping are added

---
+++ Factory Method/Model/Exercise.swift
@@ -0,0 +1,17 @@
+//
+//  Exercise.swift
+//  Factory Method
+//
+//  Created by Evgeniy Ryshkov on 18/01/2019.
+//  Copyright © 2019 Evgeniy Ryshkov. All rights reserved.
+//
+
+import Foundation
+
+protocol Exercise {
+    var name: String {get}
+    var type: String {get}
+    
+    func start()
+    func stop()
+}
---
+++ Factory Method/Model/Jumping.swift
@@ -0,0 +1,24 @@
+//
+//  Jumping.swift
+//  Factory Method
+//
+//  Created by Evgeniy Ryshkov on 18/01/2019.
+//  Copyright © 2019 Evgeniy Ryshkov. All rights reserved.
+//
+
+import Foundation
+
+class Jumping: Exercise {
+    var name: String = "Прыжки"
+    
+    var type: String = "Упражнение для ног"
+    
+    func start() {
+        <#code#>
+    }
+    
+    func stop() {
+        <#code#>
+    }
+    
+}
